331cf9de932dfdebfd0fb2df63552d33f33424d6
mariusmoldovan <biris_21@yahoo.com>
2020-08-12 12:19:17 -0500

    Done

--- app/models/band.rb
+++ app/models/band.rb
@@ -13,3 +13,7 @@ class Band
         @@all
     end
 end
+
+
+
+
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,3 +1,30 @@
 class Concert
-    
+    attr_accessor :date, :band, :venue
+   
+    @@all = []
+
+    def initialize(date, band, venue)
+        @date = date
+        @band = band
+        @venue = venue
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+    def band
+        Band.all
+    end
+    def venue
+        Venue.all
+    end
+
+
 end
+
+
+# Concert#band
+# should return the Band instance for this concert
+# Concert#venue
+# should return the Venue instance for this concert
